300ac620a6cf456211fc7c61f5d1a529aa580228
Ella Chung <echung873@gmail.com>
2025-08-05 20:11:10 -0700

    authentication ui and error handling

--- src/components/Callback.vue
+++ src/components/Callback.vue
@@ -1,14 +1,27 @@
 <template>
-    <div>
-      <h1>Authenticating...</h1>
-    </div>
-  </template>
+  <div v-if="errorOccurred" class="bg callback-container">
+    <h1>authentication failed</h1>
+    <p class="pb-4">there was an error during authentication. please try again.</p>
+    <v-btn color="primary" class="text-none" @click="goToHome">go to home</v-btn>
+  </div>
+  <div v-else class="bg callback-container">
+    <h1>Authenticating...</h1>
+    <img :src="loadingGif" alt="typing cat gif" />
+  </div>
+</template>
   
 <script>
 import { useAuthStore } from '@/stores/auth/authStore';
 import axios from 'axios';
 
 export default {
+  name: 'Callback',
+  data() {
+    return {
+      loadingGif: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExenJkaTNsZHlldTMxcHQxd2EzdnN3dGlwdXAwbXNkdnBycW1uYjV5YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4GWmvKBiml8vPQUchI/giphy.gif',
+      errorOccurred: false
+    };
+  },
   async mounted() {
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get('code'); // Get authorization code from URL
@@ -35,8 +48,29 @@ export default {
       this.$router.push('/setup');
     } catch (error) {
       console.error('Error during token exchange:', error);
+      this.errorOccurred = true;
     }
     }
-  },
+  },    
+  methods: {
+    goToHome() {
+        this.$router.push("/");
+    },
+  }
 };
 </script>
+
+<style scoped>
+.callback-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.bg {
+    background-image: url('/gradient-background.png');
+    background-size: cover;
+}
+</style>
