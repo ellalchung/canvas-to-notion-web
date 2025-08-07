5d70902d7c62404b434e216f12c4350fca6001a3
Ella Chung <echung873@gmail.com>
2025-08-06 18:47:45 -0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,56 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <Header class="header"/>
+      <div v-if="isMobile" class="bg mobile-warning">
+        <p>This app is best viewed on a desktop. Please use a web browser.</p>
+      </div>
+      <router-view v-else/>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import Header from '@/components/Header.vue';
+
+const isMobile = ref(false);
+
+const MOBILE_BREAKPOINT = 500;
+
+const checkScreenSize = () => {
+  isMobile.value = window.innerWidth < MOBILE_BREAKPOINT;
+};
+
+onMounted(() => {
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkScreenSize);
+});
 </script>
+
+<style scoped>
+.header {
+  margin: 2vh 8vw;
+  position: absolute;
+  top: 40px;
+  z-index: 2;
+}
+
+.mobile-warning {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+.bg {
+    background-image: url('/gradient-background.png');
+    background-size: cover;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <div @click="goToHome" style="cursor: pointer;">
+            <h1>course sync</h1>
+        </div>
+        <div class="header-links">
+            <h3> about </h3>
+            <h3> help </h3>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Header',
+    methods: {
+        goToHome() {
+            this.$router.push("/");
+        }
+    }
+}
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 85%;
+}
+
+.header-links {
+    display: flex;
+    gap: 50px
+}
+
+</style>
